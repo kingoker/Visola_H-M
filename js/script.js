ac8240fd2c3f6be47bf781cf982103a6de829245
Akbar_Khotamov <zolotojznak@gmail.com>
2023-03-21 23:22:26 +0500

    new model

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
 });
 
 
-// Замена левого рукава
+// Замена основы
 // Получаем ссылки на все маленькие изображения
 var baseOfJacet = document.querySelectorAll(".baseOfJacet__list .components__item img");
 
@@ -50,9 +50,49 @@ for (var i = 0; i < handsOfJacet.length; i++) {
     var handsOfJacetsmallImagePath = this.src;
 
     // Обновляем большое изображение
-    var headerOfJacetImage = document.querySelectorAll(".handsOfJacet");
-    for (var a = 0; a < headerOfJacetImage.length; a++){
-        headerOfJacetImage[a].src = handsOfJacetsmallImagePath;
-    }
+    var headerOfJacetImage = document.getElementById("handsOfJacet");
+    headerOfJacetImage.src = handsOfJacetsmallImagePath;
   });
 }
+
+
+// Замена кармана
+// Получаем ссылки на все маленькие изображения
+var pocketOfJacet = document.querySelectorAll(".pocketOfJacet__list .components__item img");
+
+// Обходим все маленькие изображения и назначаем обработчик событий для щелчка
+for (var i = 0; i < pocketOfJacet.length; i++) {
+    pocketOfJacet[i].addEventListener("click", function() {
+    // Получаем путь к маленькому изображению
+    var pocketOfJacetsmallImagePath = this.src;
+
+    // Обновляем большое изображение
+    var headerOfJacetImage = document.getElementById("pocketOfJacet");
+    headerOfJacetImage.src = pocketOfJacetsmallImagePath;
+  });
+}
+
+
+
+// Добавление и удаление классов
+// получаем все элементы с классом "components__item"
+const items = document.querySelectorAll(".components__item");
+
+// перебираем элементы и назначаем обработчик клика на каждый из них
+items.forEach((item) => {
+  item.addEventListener("click", function() {
+    // получаем родительский элемент, содержащий элемент, на который был сделан клик
+    const parentFrame = item.closest(".components__frame");
+    
+    // ищем все элементы внутри родительского элемента, кроме текущего элемента, и удаляем у них классы
+    parentFrame.querySelectorAll(".components__item").forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("components__item-active");
+      }
+    });
+    
+    // добавляем класс "components__item-active" текущему элементу
+    item.classList.add("components__item-active");
+  });
+});
+
